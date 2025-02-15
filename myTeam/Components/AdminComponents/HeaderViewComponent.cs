8d58d44245dab9970b2eb963742f7c07f106b766
phinh <phinh@ENDRR>
2025-02-15 21:28:51 +0700

    Refactor Admin views and add new ViewComponents
    Refactored `AdminController.cs` to uncomment and activate code for retrieving and displaying contacts. Updated `Details.cshtml` and `Index.cshtml` to use modern layouts with UIKit and flexbox, respectively. Added search functionality and account usage info to `Index.cshtml`.
    
    Introduced new ViewComponents: `EmailNavigationPanelViewComponent`, `HeaderViewComponent`, and `SidebarViewComponent`, each with corresponding views to enhance the admin interface with a structured email panel, header, and sidebar.
    
    Refactor Admin views and add new SVG assets
    
    - Modified `AdminController.cs` to comment out the code that retrieves and returns a list of contacts in the `Index` action method. Now, it simply returns the view without any model data.
    - Updated `Index.cshtml` to comment out the model declaration and the entire HTML table that displays the list of contacts, including table headers and rows.
    - Changed `_ViewStart.cshtml` to dynamically set the layout based on the request path. If the URL starts with "/Admin", it sets the layout to "_AdminLayout"; otherwise, it sets the layout to "_Layout".
    - Added new SVG files: `congrats.svg`, `fav.png`, `food.svg`, `sneakers.svg`, `soccer.svg`, `socks.svg`, `tailwindcss.svg`, and `user.svg`, each containing detailed graphical elements.
    - Replaced the entire content of `happy.svg` with a new SVG structure, including paths, circles, rectangles, gradients, and clip paths for a complex illustration.

---
+++ myTeam/Components/AdminComponents/HeaderViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myTeam.Components.AdminComponents
+{
+    [ViewComponent(Name = "AdminHeader")]
+    public class HeaderViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("/Views/Shared/Components/Admin/Header/_Header.cshtml");
+        }
+    }
+}
